e70b06a94b94e8b401c200d5bbf3ff51ab95aeab
jleen <jleen@saturnvalley.org>
2008-04-18 05:28:51 +0000

    Document the Hebrew calendar.
    git-svn-id: https://saturnvalley.org/svn/antikythera/trunk@703 e8952cfc-6410-0410-bd16-81fef15e32ef

--- hebrew.py
+++ hebrew.py
@@ -1,6 +1,24 @@
-# Hebrew calendar
+# Hebrew Anno Mundi calendar
 #
 # Reference: http://www.shirhadash.org/calendar/abouthcal.html
+
+
+########################
+# Units and conversions.
+
+# It's convenient to think in several sets of units.  We can talk about the
+# number of halakhim since the epoch (a halakhim is 1/1080 of an hour), or the
+# number of days:hours:halakhim, or weeks:days:hours:halakhim.  If we're just
+# identifying a day, we can talk about total days, or weeks:days.  The
+# week is a useful unit because it lets us read off the day of the week from
+# the remaining days.
+#
+# Note that, for reasons your rabbi may be able to explain, Day 1 is a day
+# and a half before the first reckoned moon, and almost a year before the
+# Fiat Lux.  The date of creation isn't of interest to us here, but the date
+# of the first moon is immensely interesting.  Notice that the length of a
+# month is not an integral number of days.  Also, the round-off rules are fun.
+# But we'll get to that later.
 
 def dhh_to_halakhim(days, hours, halakhim):
     return halakhim + 1080 * (hours + 24 * days)
@@ -16,7 +34,19 @@ def halakhim_to_wdhh(total_halakhim):
 
 day_length = dhh_to_halakhim(1, 0, 0)
 month_length = dhh_to_halakhim(29, 12, 793)
-creation = dhh_to_halakhim(2, 5, 204)
+first_moon = dhh_to_halakhim(2, 5, 204)
+
+am_epoch = 347996
+
+def wd_to_jd((weeks, days)):
+    return weeks * 7 + days + am_epoch
+
+###########
+# New year.
+
+# We calculate Rosh Hashanah, the Jewish New Year's Day, by first counting up
+# the right number of months since Day 1 (12 or 13 months per year, depending
+# on intercalations)...
 
 def months_before_year(year):
     metonic_cycles = ((year - 1) / 19) * 235
@@ -27,7 +57,12 @@ def months_before_year(year):
     return metonic_cycles + months[r]
 
 def molad_tishrei(year):
-    return creation + months_before_year(year) * month_length
+    return first_moon + months_before_year(year) * month_length
+
+# But then there are some rather complex rules by which we can "postpone"
+# Rosh Hashanah if the moon is too late in the day, or if the year would be
+# starting on a "bad" day, i.e. one which would cause certain festivals to
+# fall on forbidden days of the week, later in the year.
 
 def rosh_hashanah(year):
     molad = molad_tishrei(year)
@@ -51,8 +86,19 @@ def rosh_hashanah(year):
 def is_leap(year):
     return (year % 19) in [ 0, 3, 6, 8, 11, 14, 17 ]
 
-def wd_to_jd((weeks, days)):
-    return weeks * 7 + days + 347996
+#########
+# Pesach.
+
+# Now we can finally calculate the beginning of Passover.  It's simply the 15th
+# of Nisan, but between 1 Tishrei and 15 Nisan there are three possible
+# intercalations: a missing day in defective years, an extra day in excessive
+# years, and the leap month of Adar I in leap years (which may themselves be
+# defective, excessive, or neither).
+#
+# Excessive or defective years arise simply because of the complex round-off
+# and postponement rules for Rosh Hashanah.  The simplest way to calculate
+# them is to just compute next year's Rosh Hashanah and see how many days are
+# between now and then.
 
 days_to_pesach = 30 + 29 + 30 + 29 + 30 + 29 + 14
 
@@ -62,6 +108,9 @@ def pesach_jd(year):
     adar_i = 30 * is_leap(year)
     excess = (next_rh_jd - rosh_hashanah_jd) - (354 + adar_i)
     return rosh_hashanah_jd + adar_i + excess + days_to_pesach
+
+# Cheesy Anno Domini to Anno Mundi conversion which punts the issue of the
+# different New Years' Days by being pegged to Passover.
 
 def ad_to_am_at_pesach(ad):
     return ad + 3760
