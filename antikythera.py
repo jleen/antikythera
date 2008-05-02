96568be509fc468b9a81adbcbb136dc5e881f679
jleen <jleen@saturnvalley.org>
2008-05-02 05:20:06 +0000

    J/G/H interleaved calendars.  Phase of moon is still hokey.
    git-svn-id: https://saturnvalley.org/svn/antikythera/trunk@710 e8952cfc-6410-0410-bd16-81fef15e32ef

--- antikythera.py
+++ antikythera.py
@@ -2,6 +2,7 @@
 # computes anything and everything you might care to know about calendars
 
 import computus
+import hebrew
 
 phase_names = [ 'New', 'Waxing Crescent', 'First Quarter', 'Waxing Gibbous',
         'Full', 'Waning Gibbous', 'Last Quarter', 'Waning Crescent' ]
@@ -12,14 +13,18 @@ def interleave(year):
 
     julian = julian_calendar(year)
     gregorian = gregorian_calendar(year)
+    hebrew_cal = hebrew_calendar(hebrew.ad_to_am_at_pesach(year))
     compendium = easter_compendium(year)
 
     i_julian = 0
+    i_hebrew = 0
     i = 0
 
     while julian[i_julian][0] != start_date: i_julian += 1
     while gregorian[i][0] != start_date: i += 1
+    while hebrew_cal[i_hebrew][0] != start_date: i_hebrew += 1
     offset = i_julian - i
+    hebrew_offset = i_hebrew - i
 
     done = False
     while not done:
@@ -30,6 +35,9 @@ def interleave(year):
         print_calendar_entry(julian[i + offset])
         print 'G',
         print_calendar_entry(gregorian[i])
+        print 'H',
+        print_hebrew_calendar_entry(
+                hebrew_cal[i + hebrew_offset], gregorian[i][3])
         annotations = consult_compendium(compendium, jd)
         for a in annotations: print a + '!'
         print
@@ -50,6 +58,49 @@ def easter_compendium(year):
     for key in julian_data: compendium[key] = julian_data[key]
     return compendium
 
+hebrew_month_names = [ 'Shevat', 'Adar', 'Adar I', 'Adar II', 'Nissan',
+        'Iyar', 'Sivan' ]
+
+def hebrew_calendar(year):
+    is_leap = hebrew.is_leap(year)
+
+    jd = hebrew.shevat_jd(year)
+    adar_jd = hebrew.adar_jd(year)
+    adar_i_jd = hebrew.adar_i_jd(year)
+    nisan_jd = hebrew.nisan_jd(year)
+    iyar_jd = hebrew.iyar_jd(year)
+    sivan_jd = hebrew.sivan_jd(year)
+
+    calendar = []
+    day = 1
+    month = 0
+        
+    while True:
+        calendar += [ (jd, month, day, dol_to_pom(day - 5)) ]
+        jd += 1
+
+        if is_leap and jd == adar_i_jd:
+            month = 2
+            day = 1
+        elif is_leap and jd == adar_jd:
+            month = 3
+            day = 1
+        elif (not is_leap) and jd == adar_jd:
+            month = 1
+            day = 1
+        elif jd == nisan_jd:
+            month = 4
+            day = 1
+        elif jd == iyar_jd:
+            month = 5
+            day = 1
+        elif jd == sivan_jd:
+            break
+        else:
+            day += 1
+    
+    return calendar
+
 def julian_calendar(year):
     return calendar(
             computus.julian_to_jd((year, 2, 1)),
@@ -66,6 +117,12 @@ def is_gregorian_leap_year(year):
     if year % 100 == 0: return year % 400 == 0
     else: return year % 4 == 0
 
+# HACK!
+def dol_to_pom(day_of_lunation):
+    phase = int((day_of_lunation + 4) / 3.5)
+    if phase >= 8: phase = 0
+    return phase
+
 def calendar(initial_jd, year_data, leap):
     i_january_3 = computus.find_day(year_data, 1, 3)
     i_february_1 = computus.find_day(year_data, 2, 1)
@@ -75,8 +132,7 @@ def calendar(initial_jd, year_data, leap):
 
     calendar = []
     day_of_lunation = 32 - year_data[i_january_new_moon][1]
-    phase = int((day_of_lunation + 4) / 3.5)
-    if phase >= 8: phase = 0
+    phase = dol_to_pom(day_of_lunation)
     # HACK!
     day_of_phase = int((day_of_lunation + 4) % 3.5) + 1
 
@@ -109,6 +165,13 @@ def print_calendar_entry(entry):
     mon_name = computus.months[month][0:3]
     phase_name = phase_names[phase]
     print '%s %s %2d %s' % (day_name, mon_name, day, phase_name)
+
+def print_hebrew_calendar_entry(entry, weekday):
+    (jd, month, day, phase) = entry
+    day_name = computus.weekdays[weekday][0:3]
+    mon_name = hebrew_month_names[month]
+    phase_name = phase_names[phase]
+    print '%s %d %s %s' % (day_name, day, mon_name, phase_name)
 
 def print_calendar(calendar):
     for entry in calendar: print_calendar_entry(entry)
